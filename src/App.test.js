9d95f903e9d4fed8e73dc40d2aac5ebc2b951e40
Owen Watts <owenrhyswatts@gmail.com>
2020-03-11 19:36:06 +0000

    Updated App test text

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
     const wrapper  = shallow(<App />);
     expect(wrapper.debug()).toMatchSnapshot();
 });
