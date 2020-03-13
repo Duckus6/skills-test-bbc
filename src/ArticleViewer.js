fa9a058e597fb56a3eb072e5255038d2a0d3a3ce
Owen Watts <owenrhyswatts@gmail.com>
2020-03-13 13:53:16 +0000

    Added ControlBar functionality to ArticleViewer, updated tests

--- src/ArticleViewer.js
+++ src/ArticleViewer.js
@@ -2,14 +2,22 @@ import React, {useState} from 'react';
 import './App.css';
 import ArticleAPI from './ArticleAPI';
 import Article from './Article';
+import ControlBar from './ControlBar';
 
 function ArticleViewer() {
     const API = new ArticleAPI();
+
+    const [articleIndex,setArticleIndex] = useState(0);
     const [articles,setArticles] = useState([]);
+    const [read,setRead] = useState([]);
+
     React.useEffect(()=>{
         API.getArticles().then(
             articles=>{
                 setArticles(articles);
+                if (articles.length > 0){
+                    setArticleIndex(0);
+                }
                 console.debug(`${articles.length} articles saved to state`)
             }
         ).catch(
@@ -17,13 +25,17 @@ function ArticleViewer() {
         );
     },[]);
 
-    const [articleIndex,setArticleIndex] = useState(0);
-
+    React.useEffect(()=>{
+        const newRead = read;
+        newRead[articleIndex]= true;
+        setRead(newRead);
+    },[articleIndex]);
 
     return (
         <div className="ArticleViewer">
             {articles &&<p>We have {articles.length} Articles</p>}
             {((articles) && (articles.length>0 )) && <Article articleID={articles[articleIndex]}/>}
+            <ControlBar updateIndex={setArticleIndex} index={articleIndex} total={articles.length}/>
         </div>
     );
 }
--- src/ArticleViewer.test.js
+++ src/ArticleViewer.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import AxiosMockAdapter from 'axios-mock-adapter';
+import React from "react";
+import AxiosMockAdapter from "axios-mock-adapter";
 import {mockArticle,mockArticles,mockError} from "./fixtures/articles";
 import {shallow} from "enzyme";
 import ArticleViewer from "./ArticleViewer";
@@ -12,21 +12,23 @@ import {act} from "@testing-library/react";
 const mock = new AxiosMockAdapter(axios);
 
 
-
+let mockUseEffect = null;
 describe("the server returns 200 code",()=>{
     beforeEach(()=>{
-        mock.onGet('data/').reply(200,
+        mock.onGet("data/").reply(200,
             mockArticles
         );
         mock.onGet(`data/${mockArticles[0]}`).reply(200,
             mockArticle
         );
-        jest.spyOn(React,"useEffect").mockImplementationOnce(f=>f());
+        mockUseEffect = jest.spyOn(React,"useEffect").mockImplementationOnce(f=>f());
     });
     afterEach(()=>{
         mock.reset();
+        console.log(mockUseEffect);
+        mockUseEffect.mockImplementationOnce(f=>f());
     });
-    test('renders', async (done) => {
+    test("renders", async (done) => {
         jest.useFakeTimers();
         const wrapper = shallow(<ArticleViewer/>);
         await act(async ()=>{
@@ -36,12 +38,37 @@ describe("the server returns 200 code",()=>{
         console.log(wrapper.debug());
         expect(wrapper.debug()).toMatchSnapshot();
         done();
+    });
+
+    test("Checking an article was read",async ()=>{
+        mockUseEffect.mockImplementationOnce(f=>f());
+        jest.useFakeTimers();
+        const wrapper = shallow(<ArticleViewer/>);
+        await act(async ()=>{
+            jest.runAllImmediates()
+        });
+        console.log(wrapper.debug());
+        expect(wrapper.debug()).toMatchSnapshot();
     })
+
+});
+
+
+describe("the server returns an empty list of articles",()=>{
+    beforeEach(()=> {
+        mock.onGet("data/").reply(200,
+            []
+        );
+    });
+    test("renders with no articles",()=>{
+        const wrapper = shallow(<ArticleViewer/>);
+        expect(wrapper.debug()).toMatchSnapshot();
+    });
 });
 
 describe("the server returns 500 error",()=>{
     beforeEach(()=>{
-        mock.onGet('data/').reply(500,
+        mock.onGet("data/").reply(500,
             mockError
         );
         jest.spyOn(React,"useEffect").mockImplementationOnce(f=>f());
@@ -49,7 +76,7 @@ describe("the server returns 500 error",()=>{
     afterEach(()=>{
         mock.reset();
     });
-    test('renders', () => {
+    test("renders", () => {
         const wrapper = shallow(<ArticleViewer/>);
         expect(wrapper.debug()).toMatchSnapshot();
     })
