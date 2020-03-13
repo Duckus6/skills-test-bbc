5cf98992081a21137b4d53caa9f5fdf17954410a
Owen Watts <owenrhyswatts@gmail.com>
2020-03-13 23:23:23 +0000

    Added Post method

--- src/ArticleAPI.js
+++ src/ArticleAPI.js
@@ -25,12 +25,22 @@ export default class ArticleAPI{
     getArticle(articleId){
         return new Promise((resolve,reject)=>{
             axios.get(`data/${articleId}`).then((response)=>{
-                console.log(response.data);
                 resolve(response.data);
             }).catch(err=>{
                 //TODO Handle specific HTTP codes here
                 reject(err)});
         });
+    }
+    postRankings(rankings){
+        const newRankings = [];
+        rankings.sort( () => Math.random() - 0.5).map((ranking, i)=>{
+            newRankings.push({
+                id: ranking.id,
+                rank: i
+            })
+        });
+        //Mock Data to return to ranking page
+        return Promise.resolve(newRankings);
     }
 
 }
